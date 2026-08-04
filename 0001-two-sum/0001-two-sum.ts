f2327bb0fd77e6dd23d0707d9bedf08e0944f369
Harsh singh chouhan <160889341+Harshrajput788@users.noreply.github.com>
2026-08-04 09:13:13 +0530

    Time: 0 ms (100%), Space: 57.3 MB (37.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    const map = new Map();
+    const map = new Map<number,number>();
 
     for(let i = 0;i<nums.length;i++){
-       if(map.has(target - nums[i])){
-        return [map.get(target - nums[i]),i];
-       }
-       map.set(nums[i],i);
+        if(map.has(target - nums[i])){
+            return [map.get(target-nums[i]),i];
+        }
+        map.set(nums[i],i);
     }
-
     return [-1,-1];
 };
